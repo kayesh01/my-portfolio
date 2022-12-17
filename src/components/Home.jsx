76b20be0085674f2ca6831dbe064f40492f8b4a2
kayesh01 <shaanfarhan01@gmail.com>
2022-12-17 16:25:42 +0600

    home page updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
             I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 6 months of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
+            I have 6 months of experience building and desgining Web Sites.
+            I love to work on web application using technologies like
             React, Tailwind, Bootstarp, Next JS and MongoDB.
           </p>
 
